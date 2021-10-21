c72091cf3df1f7a021bfacce6a1f6f285395be58
thisisnew <thisisnew@naver.com>
2021-10-21 22:34:30 +0900

    암호검사기

--- chap01-eclipse/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ chap01-eclipse/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -23,4 +23,11 @@ public class PasswordStrengthMeterTest {
 		PasswordStrength result = meter.meter("ab12!@A");
 		assertEquals(PasswordStrength.NORMAL, result);
 	}
+	
+	@Test
+	void meetsOtherCriteria_except_for_number_Then_Normal() {
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab!@ABqwer");
+		assertEquals(PasswordStrength.NORMAL, result);
+	}
 }
