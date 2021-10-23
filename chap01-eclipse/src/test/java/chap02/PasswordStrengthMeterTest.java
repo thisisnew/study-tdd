a0234642d689f8521d190456d311a4da498c5d67
thisisnew <thisisnew@naver.com>
2021-10-23 11:36:52 +0900

    암호검사기

--- chap01-eclipse/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ chap01-eclipse/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -5,28 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
+	PasswordStrengthMeter meter = new PasswordStrengthMeter();
 
 	@Test
 	void meetsAllCriteria_Then_Strong() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
 		PasswordStrength result = meter.meter("ab12!@AB");
 		assertEquals(PasswordStrength.STRONG, result);
-		
+
 		PasswordStrength result2 = meter.meter("abc1!Add");
 		assertEquals(PasswordStrength.STRONG, result2);
 	}
-	
-	
+
 	@Test
 	void meetsOtherCriteria_except_for_Length_Then_Normal() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
 		PasswordStrength result = meter.meter("ab12!@A");
 		assertEquals(PasswordStrength.NORMAL, result);
 	}
-	
+
 	@Test
 	void meetsOtherCriteria_except_for_number_Then_Normal() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
 		PasswordStrength result = meter.meter("ab!@ABqwer");
 		assertEquals(PasswordStrength.NORMAL, result);
 	}
