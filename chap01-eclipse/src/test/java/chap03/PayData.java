697ac2f946d16a440ae6e785054c0cb380cd61cd
thisisnew <thisisnew@naver.com>
2021-10-24 18:07:59 +0900

    서비스 만료일 계산

--- chap01-eclipse/src/test/java/chap03/PayData.java
+++ chap01-eclipse/src/test/java/chap03/PayData.java
@@ -3,14 +3,21 @@ package test.java.chap03;
 import java.time.LocalDate;
 
 public class PayData {
+	private LocalDate firstBillngDate;
 	private LocalDate billingDate;
 	private int payAmount;
-	
-	private PayData() {}
-	
-	public PayData(LocalDate billingDate, int payAmount) {
+
+	private PayData() {
+	}
+
+	public PayData(LocalDate firstBillngDate, LocalDate billingDate, int payAmount) {
+		this.firstBillngDate = firstBillngDate;
 		this.billingDate = billingDate;
 		this.payAmount = payAmount;
+	}
+
+	public LocalDate getFirstBillngDate() {
+		return firstBillngDate;
 	}
 
 	public LocalDate getBillingDate() {
@@ -20,24 +27,29 @@ public class PayData {
 	public int getPayAmount() {
 		return payAmount;
 	}
-	
+
 	public static Builder builder() {
 		return new Builder();
 	}
-	
+
 	public static class Builder {
 		private PayData data = new PayData();
-		
+
+		public Builder firstBillngDate(LocalDate firstBillngDate) {
+			data.firstBillngDate = firstBillngDate;
+			return this;
+		}
+
 		public Builder billingDate(LocalDate billingDate) {
 			data.billingDate = billingDate;
 			return this;
 		}
-		
+
 		public Builder payAmount(int payAmount) {
 			data.payAmount = payAmount;
 			return this;
 		}
-		
+
 		public PayData build() {
 			return data;
 		}
