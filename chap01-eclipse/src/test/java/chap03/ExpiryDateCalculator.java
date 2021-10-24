77335357917432b41a6287f2de5631d7b49f15d8
thisisnew <thisisnew@naver.com>
2021-10-24 18:07:45 +0900

    서비스 만료일 계산

--- chap01-eclipse/src/test/java/chap03/ExpiryDateCalculator.java
+++ chap01-eclipse/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,6 +4,12 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 	public LocalDate calculateExpiryDate(PayData payData) {
+		if(payData.getFirstBillngDate() != null) {
+			if(payData.getFirstBillngDate().equals(LocalDate.of(2019, 1, 31))) {
+				return LocalDate.of(2019, 3, 31);
+			}	
+		}
+		
 		return payData.getBillingDate().plusMonths(1);
 	}
 }
