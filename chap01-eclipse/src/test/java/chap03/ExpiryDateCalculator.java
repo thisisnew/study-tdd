090d7920d63f8ad1d9713e343fe25464f2cabfb5
thisisnew <thisisnew@naver.com>
2021-10-24 18:17:35 +0900

    리팩토링

--- chap01-eclipse/src/test/java/chap03/ExpiryDateCalculator.java
+++ chap01-eclipse/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 	public LocalDate calculateExpiryDate(PayData payData) {
-		int addedMonths  = 1;
+		int addedMonths  = payData.getPayAmount() / 10_000;
 		if (payData.getFirstBillngDate() != null) {
 			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
 			if (payData.getFirstBillngDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
