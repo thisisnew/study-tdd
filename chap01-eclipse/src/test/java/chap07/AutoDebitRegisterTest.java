b3c12bb918f52d5890566c9559fbcdebf23c2881
thisisnew <thisisnew@naver.com>
2021-10-25 20:49:42 +0900

    대역

---
+++ chap01-eclipse/src/test/java/chap07/AutoDebitRegisterTest.java
@@ -0,0 +1,32 @@
+package test.java.chap07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static test.java.chap07.CardValidity.THEFT;
+import static test.java.chap07.CardValidity.VALID;
+
+public class AutoDebitRegisterTest {
+    private AutoDebitRegister register;
+
+    @BeforeEach
+    void setUp() {
+        CardNumberValidator validator = new CardNumberValidator();
+        AutoDebitInfoRepository repository = new JpaAutoDebitInfoRepository();
+        register = new AutoDebitRegister(validator, repository);
+    }
+
+    @Test
+    void validCard() {
+        AutoDebitReq req = new AutoDebitReq("user1", "1234123412341234");
+        RegisterResult result = this.register.register(req);
+        assertEquals(VALID, result.getValidity());
+    }
+
+    @Test
+    void theftCard() {
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+        RegisterResult result = this.register.register(req);
+        assertEquals(THEFT, result.getValidity());
+    }
+}
---
+++ chap01-eclipse/src/test/java/chap07/JpaAutoDebitInfoRepository.java
@@ -0,0 +1,13 @@
+package test.java.chap07;
+
+public class JpaAutoDebitInfoRepository implements AutoDebitInfoRepository {
+    @Override
+    public void save(AutoDebitInfo info) {
+
+    }
+
+    @Override
+    public AutoDebitInfo findOne(String userId) {
+        return null;
+    }
+}
