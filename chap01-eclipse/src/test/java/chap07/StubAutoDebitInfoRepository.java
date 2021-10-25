d0db8ee094b99f06b171dcb8bfc843313f464f3d
thisisnew <thisisnew@naver.com>
2021-10-25 20:53:13 +0900

    대역을 이용한 테스트

---
+++ chap01-eclipse/src/test/java/chap07/StubAutoDebitInfoRepository.java
@@ -0,0 +1,13 @@
+package test.java.chap07;
+
+public class StubAutoDebitInfoRepository implements AutoDebitInfoRepository {
+    @Override
+    public void save(AutoDebitInfo info) {
+
+    }
+
+    @Override
+    public AutoDebitInfo findOne(String userId) {
+        return null;
+    }
+}
